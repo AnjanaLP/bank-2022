5eac27bb33d958e5ac22ce4fa358a9a7e413b445
AnjanaLP <anjana.lpn@gmail.com>
2022-03-16 16:51:28 +0000

    tidy up file

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,35 +1,33 @@
 class Printer
+  HEADER = "date || credit || debit || balance\n"
 
   def initialize(transaction_log:)
     @transaction_log = transaction_log
   end
 
   def print_statement
-    header + statement
+    HEADER + formatted_statement
+  end
+
+  def formatted_statement
+    statement.reverse.join("\n")
   end
 
   private
 
   attr_reader :transaction_log
 
-  def header
-    "date || credit || debit || balance\n"
-  end
-
   def statement
     balance = 0
-    transactions = transaction_log.transactions.map do |transaction|
+    transaction_log.transactions.map do |transaction|
       statement = ""
       statement << "#{transaction.date} || "
       if transaction.type == :credit
-        statement << "#{'%.2f' % transaction.amount} || || "
-        balance += transaction.amount
+        statement << "#{'%.2f' % transaction.amount} || || " && balance += transaction.amount
       else
-        statement << "|| #{'%.2f' % transaction.amount} || "
-        balance -= transaction.amount
+        statement << "|| #{'%.2f' % transaction.amount} || " && balance -= transaction.amount
       end
       statement << "#{'%.2f' % balance}"
     end
-    transactions.reverse.join("\n")
   end
 end
